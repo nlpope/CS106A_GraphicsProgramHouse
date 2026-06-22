d939a89f0a030ee5d3fbe7bef39b675061fc3ffd
Noah Pope <npope@tutanota.com>
2026-06-22 10:21:31 -0500

    adding files to repo

---
+++ CS106A_GraphicsProgramHouse.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class CS106A_GraphicsProgramHouse extends GraphicsProgram
+{
+	GRect baseRect = new GRect(250, 250, 150, 75);
+	double baseRectXOrigin = baseRect.getX();
+	double baseRectYOrigin = baseRect.getY();
+	double baseRectWidth = baseRect.getWidth();
+	double baseRectHeight = baseRect.getHeight();
+	
+	public void run()
+	{
+		createBase();
+		createRoof();
+	}
+	
+	
+	private void createBase()
+	{
+		baseRect.setFilled(false);
+		baseRect.setColor(Color.PINK);
+		add(baseRect);
+		println("start x = " + baseRectXOrigin);
+		println("start y = " + baseRectYOrigin);
+	}
+	
+	
+	private void createRoof()
+	{
+		GLine roofLine1 = new GLine(
+				baseRectXOrigin, 
+				baseRectYOrigin, 
+				baseRectXOrigin + (baseRectWidth / 2), 
+				baseRectYOrigin - 50
+		);
+		roofLine1.setColor(Color.PINK);
+		add(roofLine1);
+		
+		GLine roofLine2 = new GLine(
+				baseRectXOrigin + (baseRectWidth / 2), 
+				baseRectYOrigin - 50, 
+				baseRectXOrigin + baseRectWidth, 
+				baseRectYOrigin
+		);
+		roofLine2.setColor(Color.PINK);
+		add(roofLine2);
+	}
+	
+	
+	private void createWindows()
+	{
+		
+	}
+	}
+}
